1827dadd677b1ddc6d872f7e45dbb4484a370df4
pritsen <swatisen@live.in>
2021-11-25 03:07:51 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,17 +1,13 @@
 # Shamelessly copied from http://flask.pocoo.org/docs/quickstart/
 
 from flask import Flask, render_template
-app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
-
+def index():
+    return render_template('index.html')
+    
 if __name__ == '__main__':
     app.run()
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
